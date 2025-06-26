b7775bd042b1cb83d3be31d6f260be78cb3d6746
Dominic Dietz <d0mm366@protonmail.com>
2025-06-26 22:33:34 +0200

    Removal of Level.java & UserStats.java

--- src/main/java/leveluptasks/model/User.java
+++ src/main/java/leveluptasks/model/User.java
@@ -2,15 +2,16 @@ package leveluptasks.model;
 
 public class User {
     private String name;
-    private UserStats stats;
 
     private int level;
     private int xp;
     private int xpToNextLevel;
+    private int totalXp;
 
     public User(String name) {
         setName(name);
-        stats = new UserStats();
+        setLevel(1);
+        setXp(0);
     }
 
     public String getName() {
@@ -19,9 +20,6 @@ public class User {
     public void setName(String name) {
         if(name == null || name.isBlank()) throw new IllegalArgumentException("Name darf nicht leer sein!");
         this.name = name;
-    }
-    public UserStats getStats() {
-        return stats;
     }
 
     public int getXp() {
@@ -51,13 +49,24 @@ public class User {
 
     private int calculateXpToNextLevel(int level) {
         return 50 * level * level;
+    }
 
+    private void addTotalXp(int xp) {
+        setTotalXp(getTotalXp() + xp);
+    }
+    public int getTotalXp() {
+        return this.totalXp;
+    }
+    public void setTotalXp(int totalXp) {
+        if(totalXp < 0) throw new IllegalArgumentException("XP darf nicht negativ sein!");
+        this.totalXp = totalXp;
     }
 
     public void addXp(int xp) {
         setXp(getXp() + xp);
         if(getXp() >= calculateXpToNextLevel(getLevel())) {
             setLevel(getLevel() + 1);
+            addTotalXp(getXp());
             setXp(0);
             setXpToNextLevel(calculateXpToNextLevel(getLevel()));
         }
